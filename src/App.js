d10dd9c10768cf39510f72b56d1f44977c17e095
Chinmay Pandhare <pandharechinmay882@gmail.com>
2020-05-02 20:47:53 +0800

    Add Summary mode, multiple select, download button, image support; change default JSON

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useCallback, useEffect, useRef } from "react";
 import "./App.css";
+import download from 'js-file-download';
 import _data from "./data";
 
 function App() {
@@ -7,37 +8,80 @@ function App() {
   const regList = Object.keys(data);
   const [reg, setReg] = useState(regList[0]);
   const [selBit, setSelBit] = useState(null);
+  const [isSummaryMode, setSummaryMode] = useState(false);
+  const [multiSelected, setMultiSelected] = useState([]);
+  const regData = data[reg];
+  const [info, setInfo] = useState(regData ? regData.info : null);
 
   useEffect(() => {
-    if(selBit)
+    if (regData && multiSelected.length !== 0) {
+      setInfo(regData.bits.filter(b => multiSelected.indexOf(b.index) !== -1).map(b => `bit ${b.index}: ${getInfo(b)}`))
+    } else {
+      if (!selBit) {
+        setInfo(regData ? regData.info : '')
+      } else if (selBit) {
+        setInfo(getInfo(selBit));
+      }
+    }
+  }, [selBit, multiSelected]);
+
+  useEffect(() => {
+    if(selBit && regData)
       setSelBit(regData.bits[selBit.index]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   useEffect(() => {
     setSelBit(null);
+    setMultiSelected([]);
   }, [reg])
-
-  const regData = data[reg];
 
   const handleKeyDown = useCallback((e) => {
       switch (e.key) {
         case "l":
         case "h":
         case "o":
+          if(!regData || regData.type === "image")
+            break;
+          setSummaryMode(false);
           setSelBit(getNextBit(regData.bits, e.key, selBit && selBit.index));
           break;
         case "j":
         case "k":
+          setSummaryMode(false);
           setReg(getNextReg(regList, e.key, regList.indexOf(reg)))
           break;
         case " ":
+          setSummaryMode(false);
           if (!selBit) return;
           setData((currentData) => {
             const copy = deepCopyObject(currentData);
             copy[reg].bits[selBit.index].value = getNextValue(selBit.value);
             return copy;
           });
+          break;
+        case "s":
+          if(!regData || regData.type === "image")
+            break;
+          if (isSummaryMode) {
+            setSummaryMode(false);
+            setInfo(selBit ? getInfo(selBit) : (regData ? regData.info : ''))
+          } else {
+            setSummaryMode(true);
+            if(regData) setInfo(regData.bits.map(b => `${b.index}: ${getInfo(b)}`))
+          }
+          break;
+        case "m":
+          if (selBit) {
+            if (multiSelected.indexOf(selBit.index) !== -1) {
+              setMultiSelected(m => m.filter(m => m !== selBit.index))
+            } else {
+              setMultiSelected(m => [...m, selBit.index]);
+            }
+          }
+          break;
+        case "M":
+          setMultiSelected([]);
           break;
         default:
       }
@@ -51,7 +95,10 @@ function App() {
     <div className="App">
       <div id="Header">
         $ RegInfo
-        <LoadJSON setData={setData} setReg={setReg} />
+        <div id="Buttons">
+          <DownloadJSON data={data} />
+          <LoadJSON setData={setData} setReg={setReg} />
+        </div>
       </div>
       <div id="Content">
         <div id="LeftSection">
@@ -64,14 +111,19 @@ function App() {
             </p>
           ))}
         </div>
-        <div id="RegSection">
+        <div id="RegSection" className={isSummaryMode && 'summary'}>
+        {
+          regData.type === "image"
+          ? <Image location={regData.location} />
+          : (
+          <>
           <p id="RegName" onClick={_ => setSelBit(null)}>{regData.name || "No Name"}</p>
           <div id="Bits" style={{'--cols': regData.bits.length}}>
               {regData.bits.map((b) => (
                 <BitInfo bit={b} property="index" setSelBit={setSelBit} />
               ))}
               {regData.bits.map((b) => (
-                <BitInfo selBit={selBit} bit={b} property="value" setSelBit={setSelBit} />
+                <BitInfo selBit={selBit} bit={b} property="value" setSelBit={setSelBit} multiSelected={multiSelected.indexOf(b.index) !== -1} />
               ))}
               {regData.bits.map((b) => (
                 <BitInfo bit={b} property="name" setSelBit={setSelBit} />
@@ -83,14 +135,42 @@ function App() {
           <div id="Info">
             <div>
               {
-                getLines(selBit ? (selBit.info[selBit.value] || selBit.info.join('\n')) : regData.info).map(l => <p>{l}</p>)
+                getLines(info).map(l => <p>{l}</p>)
               }
             </div>
           </div>
+          </>
+          )}
         </div>
       </div>
     </div>
   ) : <div>Loading...</div>;
+}
+
+function Image({location}) {
+  return <div className="image" style={{backgroundImage: `url(${location})`}} />
+}
+
+function DownloadJSON({data}) {
+  const downloadFile = _ => {
+    download(JSON.stringify(data, null, 2), 'data.json');
+  }
+  useEffect(() => {
+    const detectD = e => {
+      if(e.key === 'd') {
+        downloadFile();
+      }
+    }
+    document.addEventListener("keyup", detectD);
+    return _ => {
+      document.removeEventListener("keyup", detectD);
+    }
+  })
+  return (
+    <div id="DownloadJSON">
+      <button onClick={downloadFile}>$ Download JSON</button>
+    </div>
+  )
 }
 
 function LoadJSON({setData, setReg}) {
@@ -136,11 +216,18 @@ function LoadJSON({setData, setReg}) {
   );
 }
 
-function BitInfo({bit, selBit, property, setSelBit}) {
+function getInfo({info: infoArray, value}) {
+  if (value === '1' || value === '0') {
+    return infoArray[value];
+  }
+  return infoArray.map(i => `${infoArray.indexOf(i)}: ${i}`);
+}
+
+function BitInfo({bit, selBit, property, setSelBit, multiSelected}) {
   const isSelected = selBit ? selBit.index === bit.index : false;
   return (
     <label
-      className={`bitInfo ${property} ${isSelected && 'selected'}`}
+      className={`bitInfo ${property} ${isSelected && 'selected'} ${multiSelected && 'multiSelected'}`}
       onClick={_ => setSelBit(bit)}>
       {bit[property]}
     </label>
@@ -164,11 +251,14 @@ function deepCopyObject(obj) {
 }
 
 function getLines(text) {
+  if (Array.isArray(text)) return text;
+  if (typeof text !== 'string') return [''];
   return text.split("\n");
 }
 
 function getNextBit(bits, pressedKey, currentIndex) {
   let nextIndex;
+  if (!bits) return;
   switch (pressedKey) {
     case 'l':
       if (currentIndex == null) currentIndex = -1;
